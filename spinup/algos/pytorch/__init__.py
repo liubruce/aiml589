c130e3aee00d521550fd44ea5cc60da0d1ba1362
liuguoq <brucelgq.liu@gmail.com>
2022-03-03 13:43:36 +1300

    temporally comments the ppg code.

--- spinup/algos/pytorch/__init__.py
+++ spinup/algos/pytorch/__init__.py
@@ -4,12 +4,12 @@
 from .ddpg.ddpg import ddpg
 from .sac.sac import sac
 from .td3.td3 import td3
-from .ppg.train import train_fn as ppg
+# from .ppg.train import train_fn as ppg
 
 DDPG = ddpg
 SAC = sac
 TD3 = td3
-PPG = ppg
+# PPG = ppg
 # ED2 = ed2
 
 # SUNRISE = sunrise

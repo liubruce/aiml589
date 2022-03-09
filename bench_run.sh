a61df0009862150f8ae81f2924433c7d19a9033e
liuguoq <brucelgq.liu@gmail.com>
2022-03-09 15:02:14 +1300

    submit jobs.

--- bench_run.sh
+++ bench_run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while read alg; do
   while read problem; do
-      # qsub -t 1-10:1 bench_2.sh $alg $problem
+      qsub -t 1-10:1 bench_2.sh $alg $problem
       echo "show: $alg $problem ..."
   done < problems.txt
   echo "$alg"

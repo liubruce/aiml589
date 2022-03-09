5ca5432c490985b9b233ec69e617181b95e0a4fd
liuguoq <brucelgq.liu@gmail.com>
2022-03-09 14:55:57 +1300

    check if get the right parameters.

--- bench_run.sh
+++ bench_run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 while read alg; do
   while read problem; do
-       # qsub qsub -t 1-10:1 bench_2.sh $alg $problem
-       echo "show: $alg $problem ..."
+      # qsub -t 1-10:1 bench_2.sh $alg $problem
+      echo "show: $alg $problem ..."
   done < problems.txt
+  echo "$alg"
 done < algorithms.txt

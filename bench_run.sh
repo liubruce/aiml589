8bd2b0d1fe4ca359cc4dbfad41036aada1545e46
liuguoq <brucelgq.liu@gmail.com>
2022-03-09 15:38:57 +1300

    commont echo

--- bench_run.sh
+++ bench_run.sh
@@ -2,7 +2,7 @@
 while read alg; do
   while read problem; do
       qsub -t 1-10:1 bench_2.sh $alg $problem
-      echo "show: $alg $problem ..."
+      # echo "show: $alg $problem ..."
   done < problems.txt
   echo "$alg"
 done < algorithms.txt

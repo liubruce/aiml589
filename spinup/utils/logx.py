46d113b2467e4df6bdec6ff504f11c4861e29934
Bruce Liu <brucelgq.liu@gmail.com>
2022-05-20 17:11:55 +1200

    add new algorithms.

--- spinup/utils/logx.py
+++ spinup/utils/logx.py
@@ -354,3 +354,27 @@ class EpochLogger(Logger):
         v = self.epoch_dict[key]
         vals = np.concatenate(v) if isinstance(v[0], np.ndarray) and len(v[0].shape)>0 else v
         return mpi_statistics_scalar(vals)
+
+def restore_tf_graph(sess, fpath):
+    """
+    Loads graphs saved by Logger.
+    Will output a dictionary whose keys and values are from the 'inputs'
+    and 'outputs' dict you specified with logger.setup_tf_saver().
+    Args:
+        sess: A Tensorflow session.
+        fpath: Filepath to save directory.
+    Returns:
+        A dictionary mapping from keys to tensors in the computation graph
+        loaded from ``fpath``.
+    """
+    tf.saved_model.loader.load(
+                sess,
+                [tf.saved_model.tag_constants.SERVING],
+                fpath
+            )
+    model_info = joblib.load(osp.join(fpath, 'model_info.pkl'))
+    graph = tf.get_default_graph()
+    model = dict()
+    model.update({k: graph.get_tensor_by_name(v) for k,v in model_info['inputs'].items()})
+    model.update({k: graph.get_tensor_by_name(v) for k,v in model_info['outputs'].items()})
+    return model

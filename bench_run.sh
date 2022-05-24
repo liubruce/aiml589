c098b142b14569db458aba30c78449fd1e3546fc
Bruce Liu <brucelgq.liu@gmail.com>
2022-05-25 11:20:29 +1200

    10 different seeds

--- bench_run.sh
+++ bench_run.sh
@@ -2,7 +2,7 @@
 # qsub -t 1-2:1 bench_2.sh ed2_on_mujoco Hopper-v3
 while read alg; do
   while read problem; do
-      qsub -t 1-5:1 bench_2.sh $alg $problem
+      qsub -t 1-10:1 bench_2.sh $alg $problem
       echo "show: $alg $problem ..."
   done < problems.txt
   echo "$alg"

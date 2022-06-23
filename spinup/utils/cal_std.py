a5828c0d70a31a25f4f9fba51b1d5ed7b4bda76b
liuguoq <brucelgq.liu@gmail.com>
2022-06-23 16:18:54 +1200

    cal confidence.

--- spinup/utils/cal_std.py
+++ spinup/utils/cal_std.py
@@ -1,7 +1,8 @@
 import numpy as np
+from scipy.stats import t
 #Peg
 # Ant, halfcheetah, hopper, humanoid, humanoidstandup, reacher, swimmer, walker2d
-a = np.array([
+x = np.array([
     [6160, 5810, 6050, 5960, 6120],
     [9310, 9230, 9250,9250,9230],
     [3470,3060,3510,1700,3510],
@@ -12,15 +13,22 @@ a = np.array([
 )
 
 # a = np.array([[1, 2], [3, 4]])
-result = np.around(np.std(a, axis=1),decimals=0)
-print('PEG std are ', result)
+result = np.around(np.std(x, axis=1),decimals=0)
 
-# ED2
-a = np.array([
-    [6040,5990,5500,6040,5700], [9300,9290,9290,9330,9150], [3420,3470,3200,1030,3470],[6190,7070,6750,7000,7020],
-    [141000,156000,156000,156000,155000], [-13.3,-14.2,-14.9,-15.2,-13.9], [89.7,87.5,86.6,93.6,85.9],
-    [4920,755,4910,5380,4810]]
-)
-result = np.std(a, axis=1)
-print('ED2 std are ', result)
+m = x.mean(axis=1)
+s = x.std(axis=1)
+dof = len(x)-1
+confidence = 0.95
+t_crit = np.abs(t.ppf((1-confidence)/2,dof))
+print((m-s*t_crit/np.sqrt(len(x)), m+s*t_crit/np.sqrt(len(x))))
+# print('PEG std are ', result)
+#
+# # ED2
+# a = np.array([
+#     [6040,5990,5500,6040,5700], [9300,9290,9290,9330,9150], [3420,3470,3200,1030,3470],[6190,7070,6750,7000,7020],
+#     [141000,156000,156000,156000,155000], [-13.3,-14.2,-14.9,-15.2,-13.9], [89.7,87.5,86.6,93.6,85.9],
+#     [4920,755,4910,5380,4810]]
+# )
+# result = np.std(a, axis=1)
+# print('ED2 std are ', result)
 
